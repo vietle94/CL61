50727096f4c1c6fd93df22dd750907294900bebb
Viet <viet.letranhoang@gmail.com>
2025-02-17 03:36:04 +0200

    add noise_filter

--- src/cl61/func/noise.py
+++ src/cl61/func/noise.py
@@ -37,3 +37,31 @@ def noise_detection(df, wavelet="bior1.1"):
     df["p_pol_smooth"] = (["range"], filtered)
     df.attrs["minimax_thresh"] = minimax_thresh
     return df
+
+
+def noise_filter(profile, wavelet="bior1.5"):
+    # wavelet transform
+    n_pad = (len(profile) // 2**7 + 3) * 2**7 - len(profile)
+    coeff = pywt.swt(
+        np.pad(
+            profile,
+            (n_pad - n_pad // 2, n_pad // 2),
+            "constant",
+            constant_values=(0, 0),
+        ),
+        wavelet,
+        trim_approx=True,
+        # level=7,
+        level=8,
+    )
+    minimax_thresh = (
+        np.median(np.abs(coeff[1]))
+        / 0.6745
+        * (0.3936 + 0.1829 * np.log2(len(coeff[1])))
+    )
+    coeff[1:] = (
+        pywt.threshold(i, value=minimax_thresh, mode="hard") for i in coeff[1:]
+    )
+    filtered = pywt.iswt(coeff, wavelet)
+    filtered = filtered[(n_pad - n_pad // 2) : len(profile) + (n_pad - n_pad // 2)]
+    return filtered
