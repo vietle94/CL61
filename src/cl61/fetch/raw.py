32bb9485fb8d9152e498926f099e205567c8580a
Viet <viet@lv20lk.wlan.fmi.fi>
2025-02-19 16:19:03 +0200

    fetch_raw

--- src/cl61/fetch/raw.py
+++ src/cl61/fetch/raw.py
@@ -1,5 +1,9 @@
 import requests
-import os
+import io
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from itertools import repeat
+import time
+import xarray as xr
 
 
 def fetch_raw(site, start_date, end_date, save_path):
@@ -12,14 +16,47 @@ def fetch_raw(site, start_date, end_date, save_path):
         "instrument": "cl61d",
     }
     metadata = requests.get(url, params).json()
-    for row in metadata:
-        file_name = row["filename"]
-        file_path = save_path + "/" + row["filename"]
-        if os.path.isfile(file_path):
-            print("Done")
-            continue
-        if "live" in file_name:
-            print(file_name)
-            res = requests.get(row["downloadUrl"])
-            with open(file_path, "wb") as f:
-                f.write(res.content)
+    # for row in metadata:
+    #     raw(row, save_path)
+    #     break
+    with ThreadPoolExecutor() as exe:
+        exe.map(raw, metadata, repeat(save_path))
+
+
+def raw(row, save_path):
+    if "live" in row["filename"]:
+        i = 0
+        if int(row["size"]) < 100000:
+            return None
+        while True:
+            try:
+                print(row["filename"])
+                bad_file = False
+                res = requests.get(row["downloadUrl"])
+                file_name = save_path + "/" + row["filename"]
+                with open(file_name, "wb") as f:
+                    f.write(res.content)
+            except ValueError as error:
+                i += 1
+                print(i)
+                if i > 50:
+                    print("skip")
+                    break
+                print(error)
+                time.sleep(1)
+                continue
+            except (OSError, KeyError):
+                bad_file = True
+                print("Bad file")
+                break
+            break
+        if bad_file:
+            return None
+
+
+def response_raw(res):
+    """return data in netcdf"""
+    df = xr.open_groups(io.BytesIO(res.content))
+    if "/diagnostics" in df.keys():
+        df["/"] = df["/"].swap_dims({"profile": "time"})
+    return df["/"]
