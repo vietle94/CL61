416908fa0949fb6e7dd64bcc64ea8de8d4b1cde5
Viet <viet@lv20lk.wlan.fmi.fi>
2025-03-08 19:54:59 +0200

    wavelet decomp on calibration profiles

--- plot_cal.py
+++ plot_cal.py
@@ -3,73 +3,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import matplotlib as mpl
-from cl61.func.noise import noise_filter
+import string
+import numpy as np
+import pywt
+
 
 # %%
-site = "Kenttarova"
-file_dir = f"/media/viet/CL61/Calibration/{site}/Summary/Data_merged/"
-df_signal = xr.open_mfdataset(glob.glob(file_dir + "*_signal.nc"))
-df_diag = xr.open_mfdataset(glob.glob(file_dir + "*_diag.nc"))
+def noise_filter(profile, wavelet="bior1.5"):
+    # wavelet transform
+    level = 8
+    n_pad = (len(profile) // 2**level + 4) * 2**level - len(profile)
+    coeff = pywt.swt(
+        np.pad(
+            profile,
+            (n_pad - n_pad // 2, n_pad // 2),
+            "constant",
+            constant_values=(0, 0),
+        ),
+        wavelet,
+        trim_approx=True,
+        # level=7,
+        level=level,
+    )
 
-df_diag = df_diag.reindex(time=df_signal.time.values, method="nearest", tolerance="8s")
-df_diag = df_diag.dropna(dim="time")
-
-df = df_diag.merge(df_signal, join="inner")
-
-df["ppol_r"] = df.p_pol / (df.range**2)
-df["xpol_r"] = df.x_pol / (df.range**2)
-temp_range = np.arange(
-    np.floor(df.internal_temperature.min(skipna=True).values),
-    np.ceil(df.internal_temperature.max(skipna=True)).values + 1,
-)
-df["ppol_r"] = df.p_pol / (df.range**2)
-df["xpol_r"] = df.x_pol / (df.range**2)
-
-df_gr = df.groupby_bins("internal_temperature", temp_range)
-df_mean = df_gr.mean(dim="time", skipna=True)
-df_std = df_gr.std(dim="time", skipna=True)
-df_count = df_gr.count(dim="time")
-df_mean = df_mean.dropna(dim="internal_temperature_bins")
-df_std = df_std.dropna(dim="internal_temperature_bins")
-df_std.sel(range=20, method="nearest").ppol_r.values
+    uthresh = np.median(np.abs(coeff[1])) / 0.6745 * np.sqrt(2 * np.log(len(coeff[1])))
+    # minimax_thresh = (
+    #     np.median(np.abs(coeff[1]))
+    #     / 0.6745
+    #     * (0.3936 + 0.1829 * np.log2(len(coeff[1])))
+    # )
+    coeff[1:] = (pywt.threshold(i, value=uthresh, mode="soft") for i in coeff[1:])
+    filtered = pywt.iswt(coeff, wavelet)
+    filtered = filtered[(n_pad - n_pad // 2) : len(profile) + (n_pad - n_pad // 2)]
+    return filtered
 
 
-my_c = mpl.colormaps["RdBu_r"](
-    np.linspace(0, 1, df_mean.internal_temperature_bins.size)
-)
+# %%
+for site, lim in zip(
+    ["vehmasmaki", "hyytiala", "kenttarova"],
+    [[-1e-14, 1e-14], [-1e-14, 1e-14], [-1e-14, 1e-14]],
+):
+    files = glob.glob(f"/media/viet/CL61/calibration/{site}/merged/*.nc")
+    fig, ax = plt.subplots(1, 2, figsize=(9, 4), sharey=True, constrained_layout=True)
+    for file in files:
+        df = xr.open_dataset(file)
+        ppol = df["p_pol"].mean(dim="time") / (df["range"]) ** 2
+        xpol = df["x_pol"].mean(dim="time") / (df["range"]) ** 2
+        ppol_d = noise_filter(ppol)
+        xpol_d = noise_filter(xpol)
+        ax[0].plot(
+            ppol_d,
+            ppol.range,
+            ".",
+            label=df.time.values[0].astype("datetime64[D]").astype(str),
+        )
+        ax[1].plot(
+            xpol_d,
+            xpol.range,
+            ".",
+            label=df.time.values[0].astype("datetime64[D]").astype(str),
+        )
+        ax[0].set_xlim(lim)
+        ax[1].set_xlim([-5e-15, 5e-15])
+        df.close()
+    ax[0].set_xlabel("ppol/range^2")
+    ax[1].set_xlabel("xpol/range^2")
+    ax[0].set_ylabel("range")
 
-fig, ax = plt.subplots(2, 2, figsize=(16, 9), constrained_layout=True)
-for t, myc in zip(df_mean.internal_temperature_bins, my_c):
-    t_mean = df_mean.sel(internal_temperature_bins=t)
-    ppol_r = noise_filter(t_mean["ppol_r"])
-    xpol_r = noise_filter(t_mean["xpol_r"])
-
-    t_std = df_std.sel(internal_temperature_bins=t)
-    ax[0, 0].plot(ppol_r, t_mean.range, color=myc, label=t.values)
-    ax[0, 0].set_xlim(-1e-14, 1e-13)
-    ax[0, 0].set_xlabel(r"$ppol/range^2$")
-
-    ax[1, 0].plot(ppol_r, t_mean.range, color=myc, label=t.values)
-    ax[1, 0].set_xlim(-1e-14, 1.5e-13)
-    ax[1, 0].set_xlabel(r"$ppol/range^2$")
-    ax[1, 0].set_ylim([0, 500])
-
-    ax[0, 1].plot(xpol_r, t_mean.range, color=myc, label=t.values)
-    ax[0, 1].set_xlim(-1e-14, 1e-13)
-    ax[0, 1].set_xlabel(r"$xpol/range^2$")
-
-    ax[1, 1].plot(xpol_r, t_mean.range, color=myc, label=t.values)
-    ax[1, 1].set_xlim(-1e-14, 1.5e-13)
-    ax[1, 1].set_xlabel(r"$xpol/range^2$")
-    ax[1, 1].set_ylim([0, 500])
-
-for ax_ in ax.flatten():
-    ax_.legend()
-    ax_.grid()
-fig.savefig(
-    f"/media/viet/CL61/Calibration/{site}/Summary/" + "internalT.png",
-    bbox_inches="tight",
-    dpi=600,
-)
+    for ax_ in ax.flatten():
+        ax_.grid()
+        ax_.legend()
+    fig.savefig(
+        f"/media/viet/CL61/img/calibration_noise_{site}.png", dpi=600, bbox_inches="tight")
 
 # %%
