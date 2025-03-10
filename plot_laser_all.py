96259a93cd4a8e0d8cdc3b036f635331984f0142
Viet <viet@lv20lk.wlan.fmi.fi>
2025-03-10 17:32:53 +0200

    modified laser all plot

--- plot_laser_all.py
+++ plot_laser_all.py
@@ -52,7 +52,9 @@ def read_noise(site, time=("22:00", "23:59")):
 
 
 # %%
-fig, axes = plt.subplots(2, 2, figsize=(9, 6), constrained_layout=True)
+fig, axes = plt.subplots(
+    2, 2, figsize=(9, 6), sharex=True, constrained_layout=True, sharey=True
+)
 for ax, site in zip(
     axes.flatten(), ["hyytiala", "kenttarova", "vehmasmaki", "lindenberg"]
 ):
@@ -83,24 +85,25 @@ for ax, site in zip(
     ax.set_ylabel(r"$\sigma²_{ppol/r^2} \times t_{integration}$")
     ax.set_xlabel("Laser power (%)")
     ax.grid()
+    ax.set_yscale("log")
     ax.set_title(site, weight="bold")
+    ax.yaxis.set_tick_params(labelbottom=True)
 
 for n, ax_ in enumerate(axes.flatten()):
     ax_.text(
         -0.0,
-        1.1,
+        1.05,
         "(" + string.ascii_lowercase[n] + ")",
         transform=ax_.transAxes,
         size=12,
     )
-axes[0, 0].set_ylim([0, 6e-27])
-axes[0, 1].set_ylim([0, 1e-25])
-axes[1, 0].set_ylim([0, 5e-27])
+ax.set_ylim([0, 1e-24])
+ax.yaxis.set_tick_params(labelbottom=True)
 fig.savefig("/media/viet/CL61/img/laser_power.png", dpi=600, bbox_inches="tight")
 
 # %%
 fig, ax = plt.subplots(
-    2, 1, figsize=(9, 6), sharex=True, sharey=True, constrained_layout=True
+    figsize=(5, 3), sharex=True, sharey=True, constrained_layout=True
 )
 site = "lindenberg"
 
@@ -112,14 +115,14 @@ df_integration["datetime"] = pd.to_datetime(df_integration["datetime"], format="
 df_integration["date"] = df_integration["datetime"].dt.date
 df_integration.drop(columns=["datetime"], inplace=True)
 
-diag = read_diag(site, time=("00:00", "22:00"))
-noise = read_noise(site, time=("00:00", "22:00"))
+diag = read_diag(site, time=("00:00", "20:00"))
+noise = read_noise(site, time=("00:00", "20:00"))
 df = diag.merge(noise, on="datetime", how="outer")
 df = df.dropna(subset=["laser_power_percent", "co_std"])
 df["date"] = df.datetime.dt.date
 df = df.merge(df_integration, on="date", how="outer")
 
-p = ax[1].scatter(
+p = ax.scatter(
     df["laser_power_percent"],
     (df["co_std"] ** 2) * df["integration"],
     c=mdates.date2num(df.datetime),
@@ -131,6 +134,10 @@ cbar.ax.yaxis.set_major_formatter(mdates.DateFormatter("%b\n%Y"))
 cbar.ax.yaxis.set_major_locator(mdates.MonthLocator(interval=6))
 cbar.ax.yaxis.set_minor_locator(mdates.MonthLocator(interval=1))
 
+ax.set_axisbelow(True)
+ax.set_ylabel(r"$\sigma²_{ppol/r^2} \times t_{integration}$")
+ax.grid(linestyle="--")
+ax.set_xlabel("Laser power (%)")
 
 diag = read_diag(site)
 noise = read_noise(site)
@@ -139,30 +146,10 @@ df = df.dropna(subset=["laser_power_percent", "co_std"])
 df["date"] = df.datetime.dt.date
 df = df.merge(df_integration, on="date", how="outer")
 
-p = ax[1].scatter(
+p = ax.scatter(
     df["laser_power_percent"], (df["co_std"] ** 2) * df["integration"], c="grey", s=1
 )
-
-p = ax[0].scatter(
-    df["laser_power_percent"],
-    (df["co_std"] ** 2) * df["integration"],
-    c=mdates.date2num(df.datetime),
-    s=1,
-)
-
-for n, ax_ in enumerate(ax.flatten()):
-    ax_.text(
-        -0.0,
-        1.1,
-        "(" + string.ascii_lowercase[n] + ")",
-        transform=ax_.transAxes,
-        size=12,
-    )
-    ax_.set_axisbelow(True)
-    ax_.set_ylabel(r"$\sigma²_{ppol/r^2} \times t_{integration}$")
-    ax_.grid(linestyle="--")
-
-ax[1].set_xlabel("Laser power (%)")
+ax.set_yscale("log")
 fig.savefig(
     "/media/viet/CL61/img/laser_power_lindenberg.png", dpi=600, bbox_inches="tight"
 )
