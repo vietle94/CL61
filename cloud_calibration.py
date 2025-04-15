5883e283e124c122285f066cb466a87a4bc4cf9e
Viet <viet@lv20lk.wlan.fmi.fi>
2025-04-15 19:17:14 +0300

    cloud calibration with etaS

--- cloud_calibration.py
+++ cloud_calibration.py
@@ -2,11 +2,12 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-from cl61.func.calibration_cloud import calibration_factor
+from cl61.func.calibration_cloud import calibration_etaS
 from matplotlib.colors import LogNorm
 import matplotlib.dates as mdates
 import string
 import pandas as pd
+
 myFmt = mdates.DateFormatter("%H:%M")
 
 # %%
@@ -19,7 +20,7 @@ df = xr.open_mfdataset(glob.glob(file_dir + "*.nc"))
 
 profile = df.sel(time=slice("2024-03-03T16:30:00", "2024-03-03T17:00:00"))
 profile = profile.sel(range=slice(400, 4000))
-c = calibration_factor(profile)
+
 p = ax[0, 0].pcolormesh(
     profile["time"],
     profile["range"],
@@ -28,17 +29,18 @@ p = ax[0, 0].pcolormesh(
 )
 fig.colorbar(p, ax=ax[0, 0], label=r"$\beta$ [m-1 sr-1]")
 
+etaS = calibration_etaS(profile)
+c = etaS / 17
+
 ax[0, 0].set_ylabel("Range [m]")
 ax[1, 0].set_ylabel("C")
 ax[1, 0].plot(c.time, c)
 
-file_dir = "/media/viet/CL61/studycase/lindenberg/20250202/"
+file_dir = "/media/viet/CL61/studycase/lindenberg/20241116/"
 df = xr.open_mfdataset(glob.glob(file_dir + "*.nc"))
 
-profile = df.sel(time=slice("2025-02-02T16:30:00", "2025-02-02T17:00:00"))
+profile = df.sel(time=slice("2024-11-16T16:00:00", "2024-11-16T18:00:00"))
 profile = profile.sel(range=slice(400, 4000))
-c = calibration_factor(profile)
-
 p = ax[0, 1].pcolormesh(
     profile["time"],
     profile["range"],
@@ -46,6 +48,9 @@ p = ax[0, 1].pcolormesh(
     norm=LogNorm(vmin=1e-7, vmax=1e-4),
 )
 fig.colorbar(p, ax=ax[0, 1], label=r"$\beta$ [m-1 sr-1]")
+etaS = calibration_etaS(profile)
+c = etaS / 17
+
 ax[1, 1].plot(c.time, c)
 
 for ax_ in ax[1, :]:
@@ -60,24 +65,22 @@ for n, ax_ in enumerate(ax.flatten()):
         transform=ax_.transAxes,
         size=12,
     )
-
+ax[0, 0].set_ylim(0, 4000)
+ax[1, 0].set_ylim(0, 3)
 fig.savefig(
     "/media/viet/CL61/img/studycase_lindenberg_c.png",
     dpi=600,
     bbox_inches="tight",
 )
 # %%
-fig, ax = plt.subplots(
-    2, 2, figsize=(9, 6), sharey=True, constrained_layout=True
-)
+fig, ax = plt.subplots(2, 3, figsize=(12, 6), sharey=True, sharex='col',
+                        constrained_layout=True)
 
 file_dir = "/media/viet/CL61/studycase/lindenberg/20240303/"
 df = xr.open_mfdataset(glob.glob(file_dir + "*.nc"))
 
 profile = df.sel(time=slice("2024-03-03T16:30:00", "2024-03-03T17:00:00"))
 profile = profile.sel(range=slice(400, 4000))
-c = calibration_factor(profile)
-
 p = ax[0, 0].pcolormesh(
     profile["time"],
     profile["range"],
@@ -87,26 +90,64 @@ p = ax[0, 0].pcolormesh(
 fig.colorbar(p, ax=ax[0, 0], label=r"$\beta$ [m-1 sr-1]")
 ax[0, 0].set_ylabel("Range [m]")
 
-ax[1, 0].plot(
-    profile["p_pol"].mean(dim="time").values, profile["range"], ".", label="2024-03-03"
-)
-ax[1, 1].plot(
-    profile["p_pol"].mean(dim="time").values, profile["range"], ".", label="2024-03-03"
+etaS = calibration_etaS(profile)
+c = etaS / 17
+c = np.median(c.values)
+
+ax[0, 2].plot(
+    profile["p_pol"].mean(dim="time").values, profile["range"], ".", label="2024-03-03",
+    zorder=10
 )
 
-file_dir = "/media/viet/CL61/studycase/lindenberg/20250202/"
+p = ax[1, 0].pcolormesh(
+    profile["time"],
+    profile["range"],
+    profile["p_pol"].T*c,
+    norm=LogNorm(vmin=1e-7, vmax=1e-4),
+)
+fig.colorbar(p, ax=ax[1, 0], label=r"$\beta$ [m-1 sr-1]")
+ax[1, 0].set_ylabel("Range [m]")
+ax[1, 2].plot(
+    profile["p_pol"].mean(dim="time").values*c, profile["range"], ".", label="2024-03-03",
+    zorder=10
+)
+
+file_dir = "/media/viet/CL61/studycase/lindenberg/20241116/"
 df = xr.open_mfdataset(glob.glob(file_dir + "*.nc"))
 
-profile = df.sel(time=slice("2025-02-02T16:30:00", "2025-02-02T17:00:00"))
+profile = df.sel(time=slice("2024-11-16T16:30:00", "2024-11-16T17:00:00"))
 profile = profile.sel(range=slice(400, 4000))
-c1 = calibration_factor(profile)
 p = ax[0, 1].pcolormesh(
     profile["time"],
     profile["range"],
-    profile["p_pol"].T * (np.median(c1.values) / np.median(c.values)),
+    profile["p_pol"].T,
     norm=LogNorm(vmin=1e-7, vmax=1e-4),
 )
 fig.colorbar(p, ax=ax[0, 1], label=r"$\beta$ [m-1 sr-1]")
+
+ax[0, 2].plot(
+    profile["p_pol"].mean(dim="time").values, profile["range"], ".", label="2024-11-16",
+    zorder=1
+)
+etaS = calibration_etaS(profile)
+c = etaS / 17
+c = np.median(c.values)
+
+p = ax[1, 1].pcolormesh(
+    profile["time"],
+    profile["range"],
+    profile["p_pol"].T * c,
+    norm=LogNorm(vmin=1e-7, vmax=1e-4),
+)
+fig.colorbar(p, ax=ax[1, 1], label=r"$\beta$ [m-1 sr-1]")
+ax[1, 2].plot(
+    profile["p_pol"].mean(dim="time").values * c,
+    profile["range"],
+    ".",
+    label="2024-11-16",
+    zorder=1,
+)
+
 
 for n, ax_ in enumerate(ax.flatten()):
     ax_.text(
@@ -116,27 +157,16 @@ for n, ax_ in enumerate(ax.flatten()):
         transform=ax_.transAxes,
         size=12,
     )
-ax[1, 0].plot(
-    profile["p_pol"].mean(dim="time").values, profile["range"], '.', label="2025-02-02"
-)
-ax[1, 1].plot(
-    profile["p_pol"].mean(dim="time").values
-    * (np.median(c1.values) / np.median(c.values)),
-    profile["range"],
-    ".",
-    label="2025-02-02",
-)
 
-for ax_ in ax[0, :]:
+for ax_ in ax[:, :-1].flatten():
     ax_.xaxis.set_major_formatter(myFmt)
 
-for ax_ in ax[1, :]:
-    ax_.set_xlabel(r"$\beta$ [m-1 sr-1]")
-    ax_.set_xscale('log')
+for ax_ in ax[:, -1].flatten():
+    # ax_.set_xscale("log")
     ax_.grid()
     ax_.legend()
-    ax_.set_xlim([1e-9, 1e-3])
-
+    ax_.set_xlim([-0.5e-6, 1e-6])
+ax[1, 2].set_xlabel(r"$\beta$ [m-1 sr-1]")
 fig.savefig(
     "/media/viet/CL61/img/studycase_lindenberg_corrected.png",
     dpi=600,
@@ -145,26 +175,29 @@ fig.savefig(
 
 # %%
 file_dir = "/media/viet/CL61/lindenberg/Cloud/"
-df = pd.concat([pd.read_csv(f) for f in glob.glob(file_dir + "*.csv")], ignore_index=True)
+df = pd.concat(
+    [pd.read_csv(f) for f in glob.glob(file_dir + "*.csv")], ignore_index=True
+)
 df["datetime"] = pd.to_datetime(df["datetime"])
 df = df.sort_values("datetime")
 
-df['mask'] = df['cross_correlation'] > 2e-6
-mask = df.set_index("datetime").rolling("1h", center=True).count()['mask'] > 12
+df["c"] = df["c"] * 0.8 * 18.8 / 5
+df["mask"] = df["cross_correlation"] > 2e-6
+mask = df.set_index("datetime").rolling("1h", center=True).count()["mask"] > 12
 mask = mask.reset_index()
 df_plot = mask.merge(df)
-df_plot = df_plot[df_plot['mask']]
+df_plot = df_plot[df_plot["mask"]]
 
 # %%
-df_mean = df_plot.set_index('datetime').resample('7d').median()
+df_mean = df_plot.set_index("datetime").resample("7d").median()
 fig, ax = plt.subplots(figsize=(6, 4))
 ax.plot(df_plot.datetime, df_plot["c"], ".")
 ax.plot(df_mean.index, df_mean["c"])
-ax.set_ylabel('C')
+ax.set_ylabel("C")
 ax.grid()
-fig.savefig(
-    "/media/viet/CL61/img/calibration_factor_lindenberg.png",
-    dpi=600,
-    bbox_inches="tight",
-)
+# fig.savefig(
+#     "/media/viet/CL61/img/calibration_factor_lindenberg.png",
+#     dpi=600,
+#     bbox_inches="tight",
+# )
 # %%
