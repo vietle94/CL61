6938f4455ed8e619c8ba46e15650876fb5d6d692
Viet <viet.letranhoang@gmail.com>
2025-04-17 22:35:27 +0300

    cloud calibration

--- src/cl61/func/calibration_cloud.py
+++ src/cl61/func/calibration_cloud.py
@@ -1,4 +1,33 @@
+import numpy as np
+import glob
+import pandas as pd
+import h5py
+
+
 def calibration_etaS(profile):
     b_integrated = 5 * profile["p_pol"].sum(dim="range")
     etaS = 1 / (2 * b_integrated)
     return etaS
+
+
+def cloud_calibration(site):
+    site_name = {
+        "vehmasmaki": "Vehmasmaki",
+        "hyytiala": "Hyytiala",
+        "kenttarova": "Kenttarova",
+        "lindenberg": "Lindeberg",
+    }
+    mat = h5py.File(
+        glob.glob(f"/media/viet/CL61/cloud_calibration/{site_name[site]}.mat")[0]
+    )
+    df = pd.DataFrame(np.array(mat.get(site_name[site])).T)
+    df.columns = [
+        "datetime",
+        "cloud_base",
+        "cloud_top",
+        "beta",
+        "etaS",
+    ]
+    df["datetime"] = pd.to_datetime(df["datetime"] - 719529, unit="D")
+    df["c"] = (1 / (2 * df["etaS"])) / df["beta"]
+    return df
