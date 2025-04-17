6032ff43533beb3fe4200121ed54196133364faa
Viet <viet.letranhoang@gmail.com>
2025-04-17 22:34:37 +0300

    cloud calibration plot bk

---
+++ plot_cloud_calibration_bk.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import glob
+from cl61.func.calibration_cloud import cloud_calibration
+import matplotlib.dates as mdates
+import string
+import pandas as pd
+
+# %%
+fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharex=True)
+for site, ax, lim in zip(
+    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
+    axes.flatten(),
+    [12, 10, 10, 3],
+):
+    cloud = cloud_calibration(site)
+    ax.scatter(cloud.datetime, cloud["c"], alpha=0.05, s=1)
+    ax.set_ylim(0, lim)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+    ax.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
+    ax.grid()
+    ax.set_ylabel("c")
+
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+fig.savefig(
+    "/media/viet/CL61/img/calibration_factor_ts.png", bbox_inches="tight", dpi=600
+)
+# %%
+fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharex=True)
+for site, ax, lim in zip(
+    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
+    axes.flatten(),
+    [12, 10, 10, 2],
+):
+    cloud = cloud_calibration(site)
+    file_dir = f"/media/viet/CL61/{site}/Noise/*.csv"
+    files = glob.glob(file_dir)
+    df = pd.concat([pd.read_csv(file) for file in files], ignore_index=True)
+    df["datetime"] = pd.to_datetime(df["datetime"])
+    df = df[df["datetime"] > "2000-01-01"]
+    df = df.set_index("datetime").between_time("21:00", "23:00").reset_index()
+    df = df.groupby("range").get_group("(6000, 8000]")
+
+    cloud = (
+        cloud.set_index("datetime")
+        .resample("5min")
+        .mean(numeric_only=True)
+        .dropna()
+        .reset_index()
+    )
+    df = (
+        df.set_index("datetime")
+        .resample("5min")
+        .mean(numeric_only=True)
+        .dropna()
+        .reset_index()
+    )
+
+    df_full = cloud.merge(df)
+    ax.scatter(df_full["co_std"], df_full["c"], alpha=0.5, s=1)
+
+    ax.set_ylim(0, lim)
+    ax.grid()
+    ax.set_ylabel("c")
+
+ax.set_xlabel("co_std")
+ax.set_xscale("log")
+
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+fig.savefig(
+    "/media/viet/CL61/img/calibration_factor_bk.png", bbox_inches="tight", dpi=600
+)
+# %%
