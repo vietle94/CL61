b17291d4295ca6e926c14f6bc4fe13bb5d302a9e
Viet <viet@lv20lk.wlan.fmi.fi>
2025-05-05 17:53:01 +0300

    add cloud calibration vs laser

--- plot_cloud_calibration_bk.py
+++ plot_cloud_calibration_bk.py
@@ -83,4 +83,66 @@ for n, ax_ in enumerate(axes.flatten()):
 fig.savefig(
     "/media/viet/CL61/img/calibration_factor_bk.png", bbox_inches="tight", dpi=600
 )
+
+
 # %%
+def read_diag(site, time=("22:00", "23:59")):
+    diag = pd.DataFrame({})
+    for file in glob.glob(f"/media/viet/CL61/{site}/Diag/*.csv"):
+        df = pd.read_csv(file)
+        try:
+            df = df[["datetime", "laser_power_percent", "internal_temperature"]]
+        except KeyError:
+            continue
+        df["datetime"] = pd.to_datetime(df["datetime"])
+        df = df[df["datetime"] > "2000-01-01"]
+        df_1h = (
+            df.set_index("datetime")
+            .between_time(*time)
+            .resample("5min")
+            .mean()
+            .reset_index()
+        )
+        diag = pd.concat([diag, df_1h])
+    diag = diag.reset_index(drop=True)
+    return diag
+
+
+# %%
+fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True)
+for site, ax, lim in zip(
+    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
+    axes.flatten(),
+    [2, 2, 2, 2],
+):
+    cloud = cloud_calibration(site)
+    diag = read_diag(site)
+
+    cloud = (
+        cloud.set_index("datetime")
+        .resample("5min")
+        .mean(numeric_only=True)
+        .dropna()
+        .reset_index()
+    )
+
+    df_full = cloud.merge(diag)
+    ax.scatter(df_full["laser_power_percent"], df_full["c"], alpha=0.5, s=1)
+
+    ax.set_ylim(0, lim)
+    ax.grid()
+    ax.set_ylabel("c")
+
+ax.set_xlabel("laser_power_percent")
+
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+fig.savefig(
+    "/media/viet/CL61/img/calibration_factor_laser.png", bbox_inches="tight", dpi=600
+)
