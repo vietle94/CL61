fdf59d91a0040aab62780d33e046bd14cd92e927
Viet <viet@lv20lk.wlan.fmi.fi>
2025-05-07 13:41:47 +0300

    add more graphs to laser and cloud calibration

--- plot_cloud_calibration_bk.py
+++ plot_cloud_calibration_bk.py
@@ -19,6 +19,7 @@ for site, ax, lim in zip(
     ax.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
     ax.grid()
     ax.set_ylabel("c")
+    break
 
 for n, ax_ in enumerate(axes.flatten()):
     ax_.text(
@@ -148,3 +149,86 @@ fig.savefig(
 )
 
 # %%
+def read_diag(site):
+    diag = pd.DataFrame({})
+    for file in glob.glob(f"/media/viet/CL61/{site}/Diag/*.csv"):
+        df = pd.read_csv(file)
+        try:
+            df = df[["datetime", "laser_power_percent", "internal_temperature"]]
+        except KeyError:
+            continue
+        df["datetime"] = pd.to_datetime(df["datetime"])
+        df = df[df["datetime"] > "2000-01-01"]
+        df_1h = (
+            df.set_index("datetime")
+            .resample("5min")
+            .mean()
+            .reset_index()
+        )
+        diag = pd.concat([diag, df_1h])
+    diag = diag.reset_index(drop=True)
+    return diag
+
+# %%
+cmap = plt.get_cmap("viridis")
+cmap.set_bad("grey")
+fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharey=True, sharex=True)
+for site, ax, lim in zip(
+    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
+    axes.flatten(),
+    [12, 10, 10, 3],
+):
+    cloud = cloud_calibration(site)
+    diag = read_diag(site)
+
+    cloud = (
+        cloud.set_index("datetime")
+        .resample("5min")
+        .mean(numeric_only=True)
+        .dropna()
+        .reset_index()
+    )
+
+    df_full = cloud.merge(diag, how="left")
+
+    p = ax.scatter(
+        df_full.datetime,
+        df_full["c"],
+        c=df_full["laser_power_percent"],
+        plotnonfinite=True,
+        vmin=0,
+        vmax=100,
+        s=1,
+        cmap=cmap,
+    )
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+    ax.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
+    ax.grid()
+    ax.set_ylabel("c")
+    ax.axhline(1, color="grey", linestyle="--")
+ax.set_xlim(right=pd.to_datetime("2024-12-30"))
+fig.colorbar(p, ax=axes.ravel().tolist(), label="Laser power (%)")
+ax_flat = axes.flatten()
+ax_flat[0].axvspan("2022-06-15", "2023-04-27", color="C0", alpha=0.2, label="1.1.10")
+ax_flat[0].axvspan("2023-04-28", "2025-01-01", color="C1", alpha=0.2, label="1.2.7")
+ax_flat[1].axvspan("2022-11-21", "2023-11-22", color="C0", alpha=0.2, label="1.1.10")
+ax_flat[1].axvspan("2023-11-23", "2025-01-01", color="C1", alpha=0.2, label="1.1.10")
+ax_flat[2].axvspan("2023-06-21", "2025-01-01", color="C1", alpha=0.2, label="1.1.7")
+ax_flat[3].axvspan("2024-03-01", "2025-01-01", color="C0", alpha=0.2, label="1.1.10")
+
+ax.set_ylim(0, 10)
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+    ax_.legend(loc="upper left")
+    
+
+fig.savefig(
+    "/media/viet/CL61/img/calibration_factor_ts2.png", bbox_inches="tight", dpi=600
+)
+
--- plot_laser_all.py
+++ plot_laser_all.py
@@ -154,3 +154,112 @@ fig.savefig(
     "/media/viet/CL61/img/laser_power_lindenberg.png", dpi=600, bbox_inches="tight"
 )
 # %%
+cmap = plt.get_cmap("viridis")
+cmap.set_bad("grey")
+fig, axes = plt.subplots(
+    4, 1, figsize=(8, 8), sharex=True, constrained_layout=True, sharey=True
+)
+for ax, site in zip(
+    axes.flatten(),
+    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
+):
+    diag = read_diag(site)
+    noise = read_noise(site)
+    df = noise.merge(diag, on="datetime", how="left")
+    # df = df.dropna(subset=["laser_power_percent", "co_std"])
+    integration_files = glob.glob(f"/media/viet/CL61/{site}/Integration/*.csv")
+    df_integration = pd.concat(
+        [pd.read_csv(x) for x in integration_files], ignore_index=True
+    )
+    df_integration["datetime"] = pd.to_datetime(
+        df_integration["datetime"], format="mixed"
+    )
+    df_integration["date"] = df_integration["datetime"].dt.date
+    df_integration.drop(columns=["datetime"], inplace=True)
+    df["date"] = df.datetime.dt.date
+    df = df.merge(df_integration, on="date", how="outer")
+    p = ax.scatter(
+        df["datetime"],
+        (df["co_std"] ** 2) * df["integration"],
+        c=df["laser_power_percent"],
+        vmin=10,
+        vmax=100,
+        plotnonfinite=True,
+        cmap=cmap,
+        s=1,
+    )
+    ax.set_ylabel(r"$\sigma²_{ppol/r^2} \times t$")
+    ax.grid()
+    ax.set_yscale("log")
+    ax.yaxis.set_tick_params(labelbottom=True)
+cbar = fig.colorbar(p, ax=axes, label="Laser power (%)")
+ax_flat = axes.flatten()
+ax_flat[0].axvspan("2022-06-15", "2023-04-27", color="C0", alpha=0.2, label="1.1.10")
+ax_flat[0].axvspan("2023-04-28", "2025-01-01", color="C1", alpha=0.2, label="1.2.7")
+ax_flat[1].axvspan("2022-11-21", "2023-11-22", color="C0", alpha=0.2, label="1.1.10")
+ax_flat[1].axvspan("2023-11-23", "2025-01-01", color="C1", alpha=0.2, label="1.1.10")
+ax_flat[2].axvspan("2023-06-21", "2025-01-01", color="C1", alpha=0.2, label="1.1.7")
+ax_flat[3].axvspan("2024-03-01", "2025-01-01", color="C0", alpha=0.2, label="1.1.10")
+
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+    ax_.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+    ax_.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
+    ax_.legend(loc="upper left")
+ax.set_ylim([0, 1e-24])
+fig.savefig("/media/viet/CL61/img/laser_power_ts.png", dpi=600, bbox_inches="tight")
+
+# %%
+fig, axes = plt.subplots(
+    2, 2, figsize=(9, 6), sharex=True, constrained_layout=True, sharey=True
+)
+for ax, site in zip(
+    axes.flatten(), ["hyytiala", "kenttarova", "vehmasmaki", "lindenberg"]
+):
+    diag = read_diag(site)
+    noise = read_noise(site)
+    df = diag.merge(noise, on="datetime", how="outer")
+    df = df.dropna(subset=["laser_power_percent", "co_std"])
+    integration_files = glob.glob(f"/media/viet/CL61/{site}/Integration/*.csv")
+    df_integration = pd.concat(
+        [pd.read_csv(x) for x in integration_files], ignore_index=True
+    )
+    df_integration["datetime"] = pd.to_datetime(
+        df_integration["datetime"], format="mixed"
+    )
+    df_integration["date"] = df_integration["datetime"].dt.date
+    df_integration.drop(columns=["datetime"], inplace=True)
+    df["date"] = df.datetime.dt.date
+    df = df.merge(df_integration, on="date", how="outer")
+    p = ax.scatter(
+        df["laser_power_percent"],
+        (df["co_std"] ** 2) * df["integration"],
+        c=df["integration"],
+        s=1,
+    )
+    cbar = fig.colorbar(p, ax=ax, label="Integration time (s)")
+    ax.set_ylabel(r"$\sigma²_{ppol/r^2} \times t_{integration}$")
+    ax.set_xlabel("Laser power (%)")
+    ax.grid()
+    ax.set_yscale("log")
+    ax.set_title(site, weight="bold")
+    ax.yaxis.set_tick_params(labelbottom=True)
+
+for n, ax_ in enumerate(axes.flatten()):
+    ax_.text(
+        -0.0,
+        1.05,
+        "(" + string.ascii_lowercase[n] + ")",
+        transform=ax_.transAxes,
+        size=12,
+    )
+ax.set_ylim([0, 1e-24])
+ax.yaxis.set_tick_params(labelbottom=True)
+fig.savefig("/media/viet/CL61/img/laser_power_t.png", dpi=600, bbox_inches="tight")
+# %%
