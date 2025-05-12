17ccc46b8f32b494e9eaa0017003a03476068787
Viet <viet@lv20lk.wlan.fmi.fi>
2025-05-12 12:04:39 +0300

    rename plot cloud calibration bk to plot cloud calibration

--- plot_cloud_calibration.py
+++ plot_cloud_calibration.py
@@ -22,34 +22,6 @@ def read_diag(site):
     diag = diag.reset_index(drop=True)
     return diag
 
-
-# %%
-fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharex=True)
-for site, ax, lim in zip(
-    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
-    axes.flatten(),
-    [12, 10, 10, 3],
-):
-    cloud = cloud_calibration(site)
-    ax.scatter(cloud.datetime, cloud["c"], alpha=0.05, s=1)
-    ax.set_ylim(0, lim)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
-    ax.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
-    ax.grid()
-    ax.set_ylabel("c")
-    break
-
-for n, ax_ in enumerate(axes.flatten()):
-    ax_.text(
-        -0.0,
-        1.05,
-        "(" + string.ascii_lowercase[n] + ")",
-        transform=ax_.transAxes,
-        size=12,
-    )
-fig.savefig(
-    "/media/viet/CL61/img/calibration_factor_ts.png", bbox_inches="tight", dpi=600
-)
 # %%
 fig, axes = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharex=True)
 for site, ax, lim in zip(
@@ -104,7 +76,7 @@ fig.savefig(
 )
 
 # %%
-fig, axes = plt.subplots(2, 2, figsize=(9, 6), constrained_layout=True)
+fig, axes = plt.subplots(2, 2, figsize=(9, 6), sharex=True, sharey=True, constrained_layout=True)
 for site, ax, lim in zip(
     ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
     axes.flatten(),
@@ -123,7 +95,7 @@ for site, ax, lim in zip(
 
     df_full = cloud.merge(diag)
 
-    ax.scatter(df_full["laser_power_percent"], df_full["c"], alpha=0.5, s=1)
+    ax.scatter(df_full["laser_power_percent"], df_full["c"], alpha=0.05, s=1)
     # # Bin the 'laser_power_percent' into bins of size 5
     # bins = pd.cut(df_full["laser_power_percent"], bins=range(0, 110, 5))
 
@@ -143,12 +115,13 @@ for site, ax, lim in zip(
     #     label="Average",
     # )
 
-    ax.set_ylim(0, lim)
+    # ax.set_ylim(0, lim)
     ax.grid()
+    ax.axhline(1, color="grey", linestyle="--")
     ax.set_ylabel("c")
 
-    ax.set_xlabel("laser_power_percent")
-
+    ax.set_xlabel("Laser power (%)")
+ax.set_ylim(0, 10)
 for n, ax_ in enumerate(axes.flatten()):
     ax_.text(
         -0.0,
@@ -157,9 +130,9 @@ for n, ax_ in enumerate(axes.flatten()):
         transform=ax_.transAxes,
         size=12,
     )
-fig.savefig(
-    "/media/viet/CL61/img/calibration_factor_laser.png", bbox_inches="tight", dpi=600
-)
+# fig.savefig(
+#     "/media/viet/CL61/img/calibration_factor_laser.png", bbox_inches="tight", dpi=600
+# )
 
 # %%
 cmap = plt.get_cmap("viridis")
@@ -205,8 +178,8 @@ ax_flat = axes.flatten()
 ax_flat[0].axvspan("2022-06-15", "2023-04-27", color="C0", alpha=0.2, label="1.1.10")
 ax_flat[0].axvspan("2023-04-28", "2025-01-01", color="C1", alpha=0.2, label="1.2.7")
 ax_flat[1].axvspan("2022-11-21", "2023-11-22", color="C0", alpha=0.2, label="1.1.10")
-ax_flat[1].axvspan("2023-11-23", "2025-01-01", color="C1", alpha=0.2, label="1.1.10")
-ax_flat[2].axvspan("2023-06-21", "2025-01-01", color="C1", alpha=0.2, label="1.1.7")
+ax_flat[1].axvspan("2023-11-23", "2025-01-01", color="C1", alpha=0.2, label="1.2.7")
+ax_flat[2].axvspan("2023-06-21", "2025-01-01", color="C1", alpha=0.2, label="1.2.7")
 ax_flat[3].axvspan("2024-03-01", "2025-01-01", color="C0", alpha=0.2, label="1.1.10")
 
 ax.set_ylim(0, 10)
@@ -222,5 +195,5 @@ for n, ax_ in enumerate(axes.flatten()):
 
 
 fig.savefig(
-    "/media/viet/CL61/img/calibration_factor_ts2.png", bbox_inches="tight", dpi=600
+    "/media/viet/CL61/img/calibration_factor_ts.png", bbox_inches="tight", dpi=600
 )
