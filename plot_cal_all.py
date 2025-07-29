1ed6bd52d1c3c51c8d46fd21f6459ea8aff93349
Viet <viet@lv20lk.dlan.fmi.fi>
2025-07-29 13:44:09 +0300

    plot calibration all temperature add labels

--- plot_cal_all.py
+++ plot_cal_all.py
@@ -28,7 +28,7 @@ plot_lim = {
     "kenttarova": [2e-14, 8e-14],
 }
 # %%
-fig, ax = plt.subplots(2, 2, figsize=(6, 6), sharey=True, sharex="col")
+fig, ax = plt.subplots(2, 2, figsize=(6, 6), sharey=True)
 for t, color in zip(t_valid, my_c):
     ax[0, 0].plot(
         df_mean.sel(internal_temperature_bins=t).ppol_ref,
@@ -60,12 +60,18 @@ for t, color in zip(t_valid, my_c):
 handles, labels = ax[0, 0].get_legend_handles_labels()
 fig.legend(handles, labels, loc="lower center", ncol=5)
 ax[0, 0].set_xlim([-5e-15, 1e-14])
-# ax[0, 0].set_xlim([-5e-14, 2e-13])
+ax[1, 0].set_xlim([-5e-15, 1e-14])
 ax[0, 1].set_xlim(plot_lim[site])
-# ax[0, 0].set_ylim([0, 1000])
+ax[1, 1].set_xlim(plot_lim[site])
+
+ax[0, 0].set_xlabel(r"$\mu_{ppol/r²}$")
+ax[0, 1].set_xlabel(r"$\sigma_{ppol/r²}$")
+ax[1, 0].set_xlabel(r"$\mu_{xpol/r²}$")
+ax[1, 1].set_xlabel(r"$\sigma_{xpol/r²}$")
+
 ax[0, 0].set_ylabel("Range [m]")
 ax[1, 0].set_ylabel("Range [m]")
-fig.subplots_adjust(bottom=0.3)
+fig.subplots_adjust(bottom=0.3, hspace=0.5)
 
 for n, ax_ in enumerate(ax.flatten()):
     ax_.text(
@@ -84,10 +90,10 @@ fig.savefig(
 # %%
 # 2000 m
 ###########################################
-site = "vehmasmaki"
-files = glob.glob(f"/media/viet/CL61/calibration/{site}/merged/*.nc")[:-1]
-df = xr.open_mfdataset(files)
-df_mean, df_std, df_count = temperature_ref(df)
+# site = "vehmasmaki"
+# files = glob.glob(f"/media/viet/CL61/calibration/{site}/merged/*.nc")[:-1]
+# df = xr.open_mfdataset(files)
+# df_mean, df_std, df_count = temperature_ref(df)
 
 # %%
 t_valid = df_count.internal_temperature_bins.where(
@@ -100,13 +106,13 @@ my_c = mpl.colormaps["RdBu_r"](
     np.append(np.linspace(0, 0.4, n1), np.linspace(0.6, 1, n2))
 )
 plot_lim = {
-    "vehmasmaki": [5e-15, 1e-14],
-    "kenttarova": [2e-14, 8e-14],
-    "hyytiala": [5e-15, 3e-14],
+    "vehmasmaki": np.array([[-5e-14, 1.5e-13], [0.5e-14, 2.5e-14]]),
+    "kenttarova": np.array([[-2e-13, 1e-13], [2e-14, 8e-14]]),
+    "hyytiala": np.array([[-7e-13, 1e-13], [5e-15, 3e-14]]),
 }
 
 # %%
-fig, ax = plt.subplots(2, 2, figsize=(6, 6), sharey=True, sharex="col")
+fig, ax = plt.subplots(2, 2, figsize=(6, 6), sharey=True)
 for t, color in zip(t_valid, my_c):
     ax[0, 0].plot(
         df_mean.sel(internal_temperature_bins=t).ppol_ref,
@@ -137,13 +143,20 @@ for t, color in zip(t_valid, my_c):
 
 handles, labels = ax[0, 0].get_legend_handles_labels()
 fig.legend(handles, labels, loc="lower center", ncol=5)
-ax[0, 0].set_xlim([-0.5e-13, 1.5e-13])
-# ax[0, 1].set_xlim(plot_lim[site])
-ax[0, 1].set_xlim([6e-15, 3e-14])
+ax[0, 0].set_xlim(plot_lim[site][0, :])
+ax[1, 0].set_xlim(plot_lim[site][0, :])
+ax[0, 1].set_xlim(plot_lim[site][1, :])
+ax[1, 1].set_xlim(plot_lim[site][1, :])
+
+ax[0, 0].set_xlabel(r"$\mu_{ppol/r²}$")
+ax[0, 1].set_xlabel(r"$\sigma_{ppol/r²}$")
+ax[1, 0].set_xlabel(r"$\mu_{xpol/r²}$")
+ax[1, 1].set_xlabel(r"$\sigma_{xpol/r²}$")
+
 ax[0, 0].set_ylim([0, 1000])
 ax[0, 0].set_ylabel("Range [m]")
 ax[1, 0].set_ylabel("Range [m]")
-fig.subplots_adjust(bottom=0.3)
+fig.subplots_adjust(bottom=0.3, hspace=0.5)
 
 for n, ax_ in enumerate(ax.flatten()):
     ax_.text(
