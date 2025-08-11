228d9bbb5f4013ddef2d5f4e6374978d55f1eee7
Viet <viet.letranhoang@gmail.com>
2025-08-11 14:15:16 +0300

    fix errors

--- plot_nearground.py
+++ plot_nearground.py
@@ -2,11 +2,6 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-import matplotlib.dates as mdates
-import string
-from scipy.ndimage import median_filter
-from scipy.stats import iqr
-from scipy.signal import ShortTimeFFT, get_window
 from matplotlib.colors import LogNorm
 import pandas as pd
 from cl61.func.near_range import analyze_noise, denoise_fft
@@ -21,9 +16,7 @@ noise = df.sel(
 )
 noise_100 = noise.sel(range=slice(None, 100))
 # ppol
-ppol_noise_filtered, outlier_mask = analyze_noise(
-    noise_100.p_pol
-)
+ppol_noise_filtered, ppol_outlier_mask = analyze_noise(noise_100.p_pol)
 noise_100["ppol_filtered"] = (("time", "range"), ppol_noise_filtered)
 noise["ppol_filtered"] = xr.concat(
     [noise_100["ppol_filtered"], noise.where(noise.range > 100, drop=True)["p_pol"]],
@@ -34,7 +27,7 @@ ppol_noise_profile = noise.sel(
 )["ppol_filtered"].mean(dim="time")
 
 # xpol
-xpol_noise_filtered, outlier_mask = analyze_noise(noise_100.x_pol)
+xpol_noise_filtered, xpol_outlier_mask = analyze_noise(noise_100.x_pol)
 noise_100["xpol_filtered"] = (("time", "range"), xpol_noise_filtered)
 noise["xpol_filtered"] = xr.concat(
     [noise_100["xpol_filtered"], noise.where(noise.range > 100, drop=True)["p_pol"]],
@@ -57,10 +50,10 @@ df_fc_100 = df_fc.sel(range=slice(None, 100))
 # ppol
 df_fc_100["ppol_filtered"] = (
     ("time", "range"),
-    denoise_fft(df_fc_100.p_pol, outlier_mask),
+    denoise_fft(df_fc_100.p_pol, ppol_outlier_mask),
 )
 df_fc["ppol_filtered"] = xr.concat(
-    [df_fc_100["ppol_filtered"], df_fc.where(noise.range > 100, drop=True)["p_pol"]],
+    [df_fc_100["ppol_filtered"], df_fc.where(df_fc.range > 100, drop=True)["p_pol"]],
     dim="range",
 )
 
@@ -84,10 +77,10 @@ df_fc["fc_ppol"] = np.exp(
 # xpol
 df_fc_100["xpol_filtered"] = (
     ("time", "range"),
-    denoise_fft(df_fc_100.x_pol, outlier_mask),
+    denoise_fft(df_fc_100.x_pol, xpol_outlier_mask),
 )
 df_fc["xpol_filtered"] = xr.concat(
-    [df_fc_100["xpol_filtered"], df_fc.where(noise.range > 100, drop=True)["p_pol"]],
+    [df_fc_100["xpol_filtered"], df_fc.where(df_fc.range > 100, drop=True)["x_pol"]],
     dim="range",
 )
 
@@ -109,52 +102,63 @@ df_fc["fc_xpol"] = np.exp(
 )
 
 # %%
-df_test =  xr.open_mfdataset(
+df_test = xr.open_mfdataset(
     glob.glob("/media/viet/CL61/studycase/kenttarova/20231002/*.nc")
 )
-
-df_test_100 = df_test.sel(
-    range=slice(None, 100))
+# df_test = df
+df_test_100 = df_test.sel(range=slice(None, 100))
 
 # ppol
 df_test_100["ppol_filtered"] = (
     ("time", "range"),
-    denoise_fft(df_test_100.p_pol, outlier_mask),
+    denoise_fft(df_test_100.p_pol, ppol_outlier_mask),
 )
 df_test["ppol_filtered"] = xr.concat(
-    [df_test_100["ppol_filtered"], df_test.where(noise.range > 100, drop=True)["p_pol"]],
+    [
+        df_test_100["ppol_filtered"],
+        df_test.where(df_test.range > 100, drop=True)["p_pol"],
+    ],
     dim="range",
 )
 
 # xpol
 df_test_100["xpol_filtered"] = (
     ("time", "range"),
-    denoise_fft(df_test_100.x_pol, outlier_mask),
+    denoise_fft(df_test_100.x_pol, xpol_outlier_mask),
 )
 df_test["xpol_filtered"] = xr.concat(
     [
         df_test_100["xpol_filtered"],
-        df_test.where(noise.range > 100, drop=True)["x_pol"],
+        df_test.where(df_test.range > 100, drop=True)["x_pol"],
     ],
     dim="range",
 )
 
-# %%
-fig, ax = plt.subplots()
-ax.pcolormesh(
+# %% Plot depo corrected
+fig, ax = plt.subplots(
+    2, 1, figsize=(12, 6), sharex=True, constrained_layout=True, sharey=True
+)
+ax[0].pcolormesh(
+    df_test.time,
+    df_test.range,
+    (df_test["x_pol"] / df_test["p_pol"]).T,
+    shading="auto",
+    vmin=0,
+    vmax=0.5,
+)
+
+ax[1].pcolormesh(
     df_test.time,
     df_test.sel(range=slice(None, 200)).range,
     (
-        (df_test.sel(range=slice(None, 200))["xpol_filtered"]
-        * df_fc["fc_xpol"])
-        / (df_test.sel(range=slice(None, 200))["ppol_filtered"]
-        * df_fc["fc_ppol"])
+        (df_test.sel(range=slice(None, 200))["xpol_filtered"] * df_fc["fc_xpol"])
+        / (df_test.sel(range=slice(None, 200))["ppol_filtered"] * df_fc["fc_ppol"])
     ).T,
     shading="auto",
     vmin=0,
     vmax=0.5,
 )
-ax.pcolormesh(
+ax[1].pcolormesh(
     df_test.time,
     df_test.sel(range=slice(200, None)).range,
     (
@@ -165,217 +169,94 @@ ax.pcolormesh(
     vmin=0,
     vmax=0.5,
 )
+ax[0].set_ylim(0, 500)
 
 # %%
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(
+    2, 1, figsize=(12, 6), sharex=True, constrained_layout=True, sharey=True
+)
+ax[0].pcolormesh(
+    df_test.sel(range=slice(None, 200)).time,
+    df_test.sel(range=slice(None, 200)).range,
+    (df_test.sel(range=slice(None, 200))["ppol_filtered"] - ppol_noise_profile).T,
+    norm=LogNorm(vmin=1e-7, vmax=1e-4),
+    shading="auto",
+)
+
+ax[1].pcolormesh(
+    df_test.sel(range=slice(None, 200)).time,
+    df_test.sel(range=slice(None, 200)).range,
+    (df_test.sel(range=slice(None, 200))["xpol_filtered"] - xpol_noise_profile).T,
+    norm=LogNorm(vmin=1e-7, vmax=1e-4),
+    shading="auto",
+)
+
+ax[0].set_ylim(0, 200)
+
+# %%
+fig, ax = plt.subplots(figsize=(12, 6), sharex=True, constrained_layout=True)
 ax.pcolormesh(
     df_test.time,
-    df_test.range,
+    df_test.sel(range=slice(None, 200)).range,
     (
-        df_test["xpol_filtered"]
-        / df_test["ppol_filtered"]
+        (
+            (df_test.sel(range=slice(None, 200))["xpol_filtered"] - xpol_noise_profile)
+            * df_fc["fc_xpol"]
+        )
+        / (
+            (df_test.sel(range=slice(None, 200))["ppol_filtered"] - ppol_noise_profile)
+            * df_fc["fc_ppol"]
+        )
     ).T,
     shading="auto",
     vmin=0,
     vmax=0.5,
 )
-
-
 # %%
-signal = df_test.p_pol
-f_signal = fft.stft(signal.values, axis=0)
-mag_signal = np.abs(f_signal)
-phase_signal = np.angle(f_signal)
-
-mag_signal_median = median_filter(mag_signal, size=(21, 1, 1))
-outlier_mask_signal = np.repeat(
-    outlier_mask[:, :, np.newaxis], f_signal.shape[2], axis=2
+depo_new = (
+    (df_test.sel(range=slice(None, 200))["xpol_filtered"] - xpol_noise_profile)
+    * df_fc["fc_xpol"]
+) / (
+    (df_test.sel(range=slice(None, 200))["ppol_filtered"] - ppol_noise_profile)
+    * df_fc["fc_ppol"]
 )
-mag_signal_filtered = np.where(outlier_mask_signal, mag_signal_median, mag_signal)
-mag_signal_filtered[:5, :, :] = mag_signal[
-    :5, :, :
-]  # keep the first frequency unchanged
-f_signal_filtered = mag_signal_filtered * np.exp(1j * phase_signal)
-signal_filtered = np.real(
-    fft.istft(f_signal_filtered, k1=signal.shape[0], f_axis=0, t_axis=2)
-)
-ppol_filtered = signal_filtered
-df_test["ppol_filtered"] = (("time", "range"), ppol_filtered)
-df_test["ppol_filtered"] = df_test.ppol_filtered - noise_profile
-
-# %%
-noise = df.sel(
-    range=slice(None, 300),
-    time=slice(
-        pd.to_datetime("2023-09-26T111500"), pd.to_datetime("2023-09-26T161000")
-    ),
-).x_pol
-# %%
-fft = ShortTimeFFT(
-    win=get_window("hamm", 256),
-    hop=128,
-    fs=10,
-)
-
-f_noise = fft.stft(noise.values, axis=0)
-mag_noise = np.abs(f_noise)
-phase_noise = np.angle(f_noise)
-
-# %%
-mag_noise_median = median_filter(mag_noise, size=(21, 1, 1))
-residual = mag_noise - mag_noise_median
-iqr_value = iqr(residual, axis=0)
-# Mark as outliers using IQR rule (e.g., 1.5x IQR)
-outlier_mask = residual > (1.5 * iqr_value)
-outlier_mask = np.max(outlier_mask, axis=2)
-outlier_mask_noise = np.repeat(outlier_mask[:, :, np.newaxis], f_noise.shape[2], axis=2)
-
-mag_noise_filtered = np.where(outlier_mask_noise, mag_noise_median, mag_noise)
-mag_noise_filtered[:5, :, :] = mag_noise[:5, :, :]  # keep the first frequency unchanged
-f_noise_filtered = mag_noise_filtered * np.exp(1j * phase_noise)
-noise_filtered = np.real(
-    fft.istft(f_noise_filtered, k1=noise.shape[0], f_axis=0, t_axis=2)
-)
-noise["noise_filtered"] = (("time", "range"), noise_filtered)
-signal = df1.x_pol
-f_signal = fft.stft(signal.values, axis=0)
-mag_signal = np.abs(f_signal)
-phase_signal = np.angle(f_signal)
-
-mag_signal_median = median_filter(mag_signal, size=(21, 1, 1))
-outlier_mask_signal = np.repeat(
-    outlier_mask[:, :, np.newaxis], f_signal.shape[2], axis=2
-)
-mag_signal_filtered = np.where(outlier_mask_signal, mag_signal_median, mag_signal)
-mag_signal_filtered[:5, :, :] = mag_signal[
-    :5, :, :
-]  # keep the first frequency unchanged
-f_signal_filtered = mag_signal_filtered * np.exp(1j * phase_signal)
-signal_filtered = np.real(
-    fft.istft(f_signal_filtered, k1=signal.shape[0], f_axis=0, t_axis=2)
-)
-xpol_filtered = signal_filtered
-df1["xpol_filtered"] = (("time", "range"), xpol_filtered)
-
-# %%
-noise_profile = noise.sel(
-    time=slice(pd.to_datetime("2023-09-26T120000"), pd.to_datetime("2023-09-26T150000"))
-)["noise_filtered"].mean(dim="time")
-denoised = (
-    df1.sel(
-        time=slice(
-            pd.to_datetime("2023-09-26T100000"), pd.to_datetime("2023-09-26T103000")
-        )
-    )["xpol_filtered"].mean(dim="time")
-    - noise_profile
+depo = (
+    df_test.sel(range=slice(None, 200))["xpol_filtered"]
+    / df_test.sel(range=slice(None, 200))["ppol_filtered"]
 )
 # %%
-ref_profile = denoised.sel(range=slice(100, 200))
-B, A = np.polyfit(ref_profile.range.values, np.log(ref_profile).values, deg=1)
-f_c_x = np.exp(
-    -(
-        np.log(denoised.sel(range=slice(None, 200)))
-        - (A + B * denoised.sel(range=slice(None, 200)).range)
-    )
-)
-
-
-# %%
-signal = df_test.x_pol
-f_signal = fft.stft(signal.values, axis=0)
-mag_signal = np.abs(f_signal)
-phase_signal = np.angle(f_signal)
-
-mag_signal_median = median_filter(mag_signal, size=(21, 1, 1))
-outlier_mask_signal = np.repeat(
-    outlier_mask[:, :, np.newaxis], f_signal.shape[2], axis=2
-)
-mag_signal_filtered = np.where(outlier_mask_signal, mag_signal_median, mag_signal)
-mag_signal_filtered[:5, :, :] = mag_signal[
-    :5, :, :
-]  # keep the first frequency unchanged
-f_signal_filtered = mag_signal_filtered * np.exp(1j * phase_signal)
-signal_filtered = np.real(
-    fft.istft(f_signal_filtered, k1=signal.shape[0], f_axis=0, t_axis=2)
-)
-xpol_filtered = signal_filtered
-df_test["xpol_filtered"] = (("time", "range"), xpol_filtered)
-df_test["xpol_filtered"] = df_test.xpol_filtered - noise_profile
-
-
+fig, ax = plt.subplots()
+ax.plot(depo_new.isel(time=5), depo_new.range)
+ax.set_xlim(0, 0.1)
 
 # %%
 fig, ax = plt.subplots()
-ax.pcolormesh(
-    df.time, df.range, (df.x_pol / df.p_pol).T, shading="auto", vmin=0, vmax=0.5
-)
-ax.set_ylim(0, 300)
+ax.plot(df_test.time, df_test.x_pol.isel(range=5), label="xpol")
+ax.plot(df_test.time, df_test.xpol_filtered.isel(range=5), label="xpol_filtered")
+ax.legend()
+ax.set_yscale("log")
+ax.set_ylim(1e-8, 1e-6)
 
 # %%
-(
-    df_test.sel(range=slice(None, 200))["xpol_filtered"]
-    * f_c_x
-    / df_test.sel(range=slice(None, 200))["ppol_filtered"]
-    * f_c_p
-).isel(time=500).plot()
-(
-    df_test.sel(range=slice(None, 200))["x_pol"]
-    / df_test.sel(range=slice(None, 200))["p_pol"]
-).isel(time=500).plot()
-
+fig, ax = plt.subplots()
+ax.plot(depo_new.time, depo_new.isel(range=5))
+ax.set_ylim(0, 0.1)
 # %%
-df = xr.open_mfdataset(glob.glob("/media/viet/CL61/studycase/kenttarova/20231002/*.nc"))
-
-signal = df.sel(range=slice(None, 300)).p_pol
-f_signal = fft.stft(signal.values, axis=0)
-mag_signal = np.abs(f_signal)
-phase_signal = np.angle(f_signal)
-
-mag_signal_median = median_filter(mag_signal, size=(21, 1, 1))
-outlier_mask_signal = np.repeat(
-    outlier_mask[:, :, np.newaxis], f_signal.shape[2], axis=2
-)
-mag_signal_filtered = np.where(outlier_mask_signal, mag_signal_median, mag_signal)
-mag_signal_filtered[:5, :, :] = mag_signal[
-    :5, :, :
-]  # keep the first frequency unchanged
-f_signal_filtered = mag_signal_filtered * np.exp(1j * phase_signal)
-signal_filtered = np.real(
-    fft.istft(f_signal_filtered, k1=signal.shape[0], f_axis=0, t_axis=2)
-)
-ppol_filtered = signal_filtered
-signal["ppol_filtered"] = (("time", "range"), ppol_filtered)
-# signal["ppol_filtered"] = signal["ppol_filtered"] - noise_profile
+fig, ax = plt.subplots()
+ax.plot(df_test.time, df_test.xpol_filtered.isel(range=5), label="xpol_filtered")
+ax.set_ylim(-1e-7, 1e-7)
 # %%
-fig, ax = plt.subplots(figsize=(8, 6), sharex=True, constrained_layout=True)
-ax.pcolormesh(
-    signal.time,
-    signal.sel(range=slice(None, 200)).range,
-    signal.sel(range=slice(None, 200)).ppol_filtered.T * f_c,
-    norm=LogNorm(vmin=1e-7, vmax=1e-4),
-    shading="auto",
+fig, ax = plt.subplots()
+ax.plot(
+    depo.sel(time=pd.to_datetime("2023-10-02T210000"), method="nearest"), depo.range
 )
-ax.pcolormesh(
-    signal.time,
-    signal.sel(range=slice(200, None)).range,
-    signal.sel(range=slice(200, None)).ppol_filtered.T,
-    norm=LogNorm(vmin=1e-7, vmax=1e-4),
-    shading="auto",
+ax.plot(
+    depo_new.sel(time=pd.to_datetime("2023-10-02T210000"), method="nearest"),
+    depo_new.range,
 )
 # %%
-fig, ax = plt.subplots(figsize=(8, 6), sharex=True, constrained_layout=True)
-ax.pcolormesh(
-    signal.time,
-    signal.sel(range=slice(None, 200)).range,
-    signal.sel(range=slice(None, 200)).T,
-    norm=LogNorm(vmin=1e-7, vmax=1e-4),
-    shading="auto",
-)
-ax.pcolormesh(
-    signal.time,
-    signal.sel(range=slice(200, None)).range,
-    signal.sel(range=slice(200, None)).T,
-    norm=LogNorm(vmin=1e-7, vmax=1e-4),
-    shading="auto",
-)
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(df_test.time, df_test.x_pol.isel(range=5), label="x_pol")
+ax.set_ylim(-1e-6, 1e-6)
+ax.grid()
 # %%
