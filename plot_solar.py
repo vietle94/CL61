5b079e540904ac65f9b918861f37bed89c4a159b
Viet <viet@lv20lk.dlan.fmi.fi>
2025-08-22 14:27:08 +0300

    add xpol profiles

--- plot_solar.py
+++ plot_solar.py
@@ -28,15 +28,18 @@ df_case_full = df_case_full.isel(range=slice(1, None))
 df_case_full = df_case_full.sel(time=slice("2024-03-05T11:00", "2024-03-05T16:00"))
 df_case = df_case_full.sel(time=slice("2024-03-05T11:00", "2024-03-05T12:00"))
 df_case["ppol_r"] = df_case.p_pol / (df_case.range**2)
+df_case["xpol_r"] = df_case.x_pol / (df_case.range**2)
 
 df_case_mean = df_case.mean(dim="time", skipna=True)
 df_case_std = df_case.std(dim="time", skipna=True)
 # profile = df_case.sel(time="2023-09-26T<10:00", method="nearest")
 
 # %%
-fig = plt.figure(layout="constrained", figsize=(8, 8))
+fig = plt.figure(layout="constrained", figsize=(10, 7))
 ax_dict = fig.subplot_mosaic(
-    [["time", "time"], ["mean_case", "mean_cal"], ["std_case", "std_cal"]],
+    [["time", "time", "time", "time"],
+     ["mean_case", "mean_case_x", "mean_cal", "mean_cal_x"], 
+     ["std_case", "std_case_x", "std_cal", "std_cal_x"]],
     sharey=True,
 )
 p = ax_dict["time"].pcolormesh(
@@ -57,55 +60,122 @@ ax_dict["mean_case"].scatter(
     radiosonde["molBsc"] / 1000 / radiosonde["range"] ** 2,
     radiosonde["range"],
     s=1,
-    label=r"$\beta_{mol}/r²$",
+    label=r"$^\parallel\beta_{mol}/r²$",
     c="C3",
 )
+ax_dict["mean_case"].set_xlabel(r"$ppol/r²$")
 ax_dict["mean_case"].set_ylabel("Range [m]")
 ax_dict["mean_case"].legend(loc="upper right")
+##################################################################
+
+ax_dict["mean_case_x"].scatter(
+    df_case_mean.xpol_r, df_case_mean.range, s=1, label=r"$\mu_{xpol/r²}$"
+)
+ax_dict["mean_case_x"].scatter(
+    radiosonde["molBsc"] / 1000 / radiosonde["range"] ** 2 / 100,
+    radiosonde["range"],
+    s=1,
+    label=r"$^\perp\beta_{mol}/r²$",
+    c="C3",
+)
+ax_dict["mean_case_x"].legend(loc="upper right")
+ax_dict["mean_case_x"].set_xlabel(r"$xpol/r²$")
+#####################################################################
 
 ax_dict["mean_cal"].scatter(
     df_mean.sel(internal_temperature_bins=18).ppol_r, df_mean.range, s=1, c="C0"
 )
 
-ax_dict["mean_cal"].scatter(
-    df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).ppol_ref,
-    df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).range,
-    s=1,
-    c="C1",
+# ax_dict["mean_cal"].scatter(
+#     df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).ppol_ref,
+#     df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).range,
+#     s=1,
+#     c="C1",
+# )
+
+# ax_dict["mean_cal"].scatter(
+#     df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).ppol_r,
+#     df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).range,
+#     s=1,
+#     c="C1",
+# )
+
+ax_dict["mean_cal"].set_xlabel(r"$ppol/r²$")
+#####################################################################
+
+ax_dict["mean_cal_x"].scatter(
+    df_mean.sel(internal_temperature_bins=18).xpol_r, df_mean.range, s=1, c="C0"
 )
 
-ax_dict["mean_cal"].scatter(
-    df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).ppol_r,
-    df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).range,
-    s=1,
-    c="C1",
-)
+# ax_dict["mean_cal_x"].scatter(
+#     df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).xpol_ref,
+#     df_mean.sel(internal_temperature_bins=18, range=slice(250, None)).range,
+#     s=1,
+#     c="C1",
+# )
 
-ax_dict["mean_cal"].set_xlabel(r"$\mu_{ppol/r²}$")
+# ax_dict["mean_cal_x"].scatter(
+#     df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).xpol_r,
+#     df_mean.sel(internal_temperature_bins=18, range=slice(None, 250)).range,
+#     s=1,
+#     c="C1",
+# )
 
+ax_dict["mean_cal_x"].set_xlabel(r"$xpol/r²$")
+#####################################################################
 
 ax_dict["std_case"].scatter(df_case_std.ppol_r, df_case_std.range, s=1)
 ax_dict["std_case"].set_xlabel(r"$\sigma_{ppol/r²}$")
 ax_dict["std_case"].set_ylabel("Range [m]")
+#####################################################################
+
+ax_dict["std_case_x"].scatter(df_case_std.xpol_r, df_case_std.range, s=1)
+ax_dict["std_case_x"].set_xlabel(r"$\sigma_{xpol/r²}$")
+ax_dict["std_case_x"].set_ylabel("Range [m]")
+#####################################################################
+
 ax_dict["std_cal"].scatter(
     df_std.sel(internal_temperature_bins=18).ppol_r,
     df_std.range,
     s=1,
 )
 
-filtered = noise_filter_std(df_std.sel(internal_temperature_bins=18).ppol_r)
-filtered[:100] = df_std.sel(internal_temperature_bins=18).ppol_r[:100].values
-ax_dict["std_cal"].scatter(
-    filtered,
+# filtered = noise_filter_std(df_std.sel(internal_temperature_bins=18).ppol_r)
+# filtered[:100] = df_std.sel(internal_temperature_bins=18).ppol_r[:100].values
+# ax_dict["std_cal"].scatter(
+#     filtered,
+#     df_std.range,
+#     s=1,
+# )
+ax_dict["std_cal"].set_xlabel(r"$\sigma_{ppol/r²}$")
+
+#####################################################################
+ax_dict["std_cal_x"].scatter(
+    df_std.sel(internal_temperature_bins=18).xpol_r,
     df_std.range,
     s=1,
 )
-ax_dict["std_cal"].set_xlabel(r"$\sigma_{ppol/r²}$")
+
+# filtered = noise_filter_std(df_std.sel(internal_temperature_bins=18).xpol_r)
+# filtered[:100] = df_std.sel(internal_temperature_bins=18).xpol_r[:100].values
+# ax_dict["std_cal_x"].scatter(
+#     filtered,
+#     df_std.range,
+#     s=1,
+# )
+ax_dict["std_cal_x"].set_xlabel(r"$\sigma_{xpol/r²}$")
+
+#####################################################################
+
 ax_dict["mean_case"].set_xlim(-1e-14, 3e-14)
+ax_dict["mean_case_x"].set_xlim(-1e-14, 3e-14)
 ax_dict["mean_cal"].set_xlim(-1e-14, 3e-14)
+ax_dict["mean_cal_x"].set_xlim(-1e-14, 3e-14)
 
 ax_dict["std_case"].set_xlim(2e-14, 6e-14)
+ax_dict["std_case_x"].set_xlim(2e-14, 6e-14)
 ax_dict["std_cal"].set_xlim(2e-14, 6e-14)
+ax_dict["std_cal_x"].set_xlim(2e-14, 6e-14)
 
 for n, ax_ in enumerate(ax_dict):
     ax_dict[ax_].text(
@@ -123,5 +193,3 @@ fig.savefig(
     dpi=600,
     bbox_inches="tight",
 )
-
-# %%
