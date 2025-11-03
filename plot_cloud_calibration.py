e7f0538697f0161947b76f6e07e9c35511fc0c1b
Viet <viet@lv20lk.dlan.fmi.fi>
2025-11-03 17:38:24 +0200

    cloud calibration

--- plot_cloud_calibration.py
+++ plot_cloud_calibration.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import glob
-from cl61.func.calibration_cloud import cloud_calibration
 import matplotlib.dates as mdates
 import string
 import pandas as pd
@@ -78,120 +77,61 @@ fig.savefig(
 )
 
 # %%
-fig, axes = plt.subplots(
-    2, 2, figsize=(6, 4), sharex=True, sharey=True, constrained_layout=True
-)
-for site, ax, lim in zip(
+fig, ax = plt.subplots(4, 1, figsize=(9, 6), constrained_layout=True, sharex=True)
+for site, ax_ in zip(
     ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
-    axes.flatten(),
-    [20, 10, 5, 2],
+    ax.flatten(),
 ):
-    cloud = cloud_calibration(site)
-    cloud = cloud[(cloud["cloud_base"] > 1000) & (cloud["cloud_base"] < 1200)]
+    cloud = pd.read_csv(
+        f"/media/viet/CL61/cloud_calibration/cloud_calibration_{site}.csv"
+    )
+    cloud["datetime"] = pd.to_datetime(cloud["datetime"])
     diag = read_diag(site)
-
-    cloud = (
-        cloud.set_index("datetime")
-        .resample("5min")
-        .mean(numeric_only=True)
-        .dropna()
-        .reset_index()
+    diag = diag[diag["datetime"] < pd.to_datetime("2025-01-01")]
+    diag["month_year"] = diag["datetime"].dt.to_period("M")
+    diag = diag.groupby(diag.month_year)["laser_power_percent"].mean()
+    diag = diag.reset_index()
+    ax_.plot(
+        cloud["datetime"],
+        cloud["calibration_factor"],
+        ".-",
+        color="C0",
     )
-
-    df_full = cloud.merge(diag)
-    ax.scatter(df_full["laser_power_percent"], df_full["c"], alpha=0.5, s=1)
-    # # Bin the 'laser_power_percent' into bins of size 5
-    # bins = pd.cut(df_full["laser_power_percent"], bins=range(0, 110, 5))
-
-    # # Group by the bins and calculate the mean of 'c' for each bin
-    # avg_line = df_full.groupby(bins)["c"].mean()
-
-    # # Calculate bin centers (the middle point of each bin)
-    # bin_centers = [bin.left + (bin.right - bin.left) / 2 for bin in avg_line.index]
-
-    # # Plot the average line using the binned values
-    # ax.plot(
-    #     bin_centers,
-    #     avg_line,
-    #     color="red",
-    #     linestyle="-",
-    #     linewidth=2,
-    #     label="Average",
-    # )
-
-    # ax.set_ylim(0, lim)
-    ax.grid()
-    ax.axhline(1, color="grey", linestyle="--")
-
-axes[0, 0].set_ylabel("c")
-axes[1, 0].set_ylabel("c")
-axes[1, 0].set_xlabel("Laser power (%)")
-axes[1, 1].set_xlabel("Laser power (%)")
-ax.set_ylim(0, 2)
-
-for n, ax_ in enumerate(axes.flatten()):
-    ax_.text(
-        -0.0,
-        1.05,
-        "(" + string.ascii_lowercase[n] + ")",
-        transform=ax_.transAxes,
-        size=12,
+    ax_.set_ylabel("c", color="C0")
+    ax_.tick_params(axis="y", labelcolor="C0")
+    ax_.grid()
+    ax_2 = ax_.twinx()
+    ax_2.plot(
+        diag["month_year"].dt.to_timestamp(),
+        diag["laser_power_percent"],
+        ".-",
+        color="C1",
     )
-fig.savefig(
-    "/media/viet/CL61/img/calibration_factor_laser.png", bbox_inches="tight", dpi=600
+    ax_2.set_ylabel("Laser power (%)", color="C1")
+    ax_2.tick_params(axis="y", labelcolor="C1")
+    ax_2.set_ylim(0, 105)
+    ax_.axhline(1, color="grey", linestyle="--")
+ax_flat = ax.flatten()
+ax_flat[0].axvspan(
+    "2022-06-15", "2023-04-27", color="tab:brown", alpha=0.2, label="1.1.10"
 )
-
-# %%
-cmap = plt.get_cmap("viridis")
-cmap.set_bad("grey")
-fig, axes = plt.subplots(
-    4, 1, figsize=(9, 6), constrained_layout=True, sharey=True, sharex=True
+ax_flat[0].axvspan(
+    "2023-04-28", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7"
 )
-for site, ax, lim in zip(
-    ["vehmasmaki", "hyytiala", "kenttarova", "lindenberg"],
-    axes.flatten(),
-    [12, 10, 10, 3],
-):
-    cloud = cloud_calibration(site)
-    cloud = cloud[(cloud["cloud_base"] > 1000) & (cloud["cloud_base"] < 1200)]
-    diag = read_diag(site)
-
-    cloud = (
-        cloud.set_index("datetime")
-        .resample("5min")
-        .mean(numeric_only=True)
-        .dropna()
-        .reset_index()
-    )
-
-    df_full = cloud.merge(diag, how="left")
-    p = ax.scatter(
-        df_full.datetime,
-        df_full["c"],
-        c=df_full["laser_power_percent"],
-        plotnonfinite=True,
-        vmin=0,
-        vmax=100,
-        s=1,
-        cmap=cmap,
-    )
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
-    ax.xaxis.set_major_locator(mdates.MonthLocator([6, 12]))
-    ax.grid()
-    ax.set_ylabel("c")
-    ax.axhline(1, color="grey", linestyle="--")
-ax.set_xlim(right=pd.to_datetime("2024-12-30"))
-fig.colorbar(p, ax=axes.ravel().tolist(), label="Laser power (%)")
-ax_flat = axes.flatten()
-ax_flat[0].axvspan("2022-06-15", "2023-04-27", color="tab:brown", alpha=0.2, label="1.1.10")
-ax_flat[0].axvspan("2023-04-28", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7")
-ax_flat[1].axvspan("2022-11-21", "2023-11-22", color="tab:brown", alpha=0.2, label="1.1.10")
-ax_flat[1].axvspan("2023-11-23", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7")
-ax_flat[2].axvspan("2023-06-21", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7")
-ax_flat[3].axvspan("2024-03-01", "2025-01-01", color="tab:brown", alpha=0.2, label="1.1.10")
-
-ax.set_ylim(0, 10)
-for n, ax_ in enumerate(axes.flatten()):
+ax_flat[1].axvspan(
+    "2022-11-21", "2023-11-22", color="tab:brown", alpha=0.2, label="1.1.10"
+)
+ax_flat[1].axvspan(
+    "2023-11-23", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7"
+)
+ax_flat[2].axvspan(
+    "2023-06-21", "2025-01-01", color="tab:purple", alpha=0.2, label="1.2.7"
+)
+ax_flat[3].axvspan(
+    "2023-07-01", "2025-01-01", color="tab:brown", alpha=0.2, label="1.1.10"
+)
+ax_.set_xlim(right=pd.to_datetime("2024-12-31"))
+for n, ax_ in enumerate(ax.flatten()):
     ax_.text(
         -0.0,
         1.05,
@@ -200,10 +140,7 @@ for n, ax_ in enumerate(axes.flatten()):
         size=12,
     )
     ax_.legend(loc="upper left")
-
-
 fig.savefig(
     "/media/viet/CL61/img/calibration_factor_ts.png", bbox_inches="tight", dpi=600
 )
-
 # %%
