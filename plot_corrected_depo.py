0fa97c224d18c2551c94e27f19172a471fb7ca95
Viet Le <viet.le@helsinki.fi>
2025-11-27 13:50:03 +0200

    new case

--- plot_corrected_depo.py
+++ plot_corrected_depo.py
@@ -4,42 +4,53 @@ import glob
 import xarray as xr
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from cl61.func.calibration_T import temperature_ref
 import string
 from cl61.func.study_case import process_raw, background_noise
 from matplotlib.colors import LogNorm
 import matplotlib.ticker as mtick
+from cl61.func import rayleigh
 
 myFmt = mdates.DateFormatter("%Y\n%m-%d\n%H:%M")
 
 # %%
-file_dir = "/media/viet/CL61/studycase/kenttarova/20230918/"
-df_sample = process_raw(file_dir, "20230918 050000", "20230918 070100")
-bk_noise = background_noise("kenttarova", "20230918")  # same time frame
+date = "20240604"
+time_slice = slice("2024-06-04T13:00", "2024-06-04T17:00")
+file_dir = f"/media/viet/CL61/studycase/kenttarova/{date}/"
+df_sample = process_raw(file_dir, f"{date} 130000", f"{date} 170000")
+bk_noise = background_noise("kenttarova", date)  # same time frame
 bk_noise = (
     bk_noise.set_index("datetime")
-    .between_time("05:00", "07:00")
+    .between_time("13:00", "17:00")
     .reset_index()
     .mean(numeric_only=True)
 )
 # %%
-files = glob.glob("/media/viet/CL61/calibration/kenttarova/merged/*.nc")
-df = xr.open_mfdataset(files)
-df_mean, df_std, _ = temperature_ref(df)
-
-df_mean_ref_sample = df_mean.sel(
-    internal_temperature_bins=df_sample.internal_temperature_bins
+ref_mean = xr.open_dataset(
+    "/media/viet/CL61/calibration/result/kenttarova/calibration_mean.nc"
+)
+df_mean_ref_sample = ref_mean.interp(
+    internal_temperature_bins=df_sample.internal_temperature_bins,
+    method="linear",
+    kwargs={"fill_value": "extrapolate", "bounds_error": False},
 ).drop_vars("internal_temperature_bins")
-df_std_ref_sample = df_std.sel(
-    internal_temperature_bins=df_sample.internal_temperature_bins
+ref_std = xr.open_dataset(
+    "/media/viet/CL61/calibration/result/kenttarova/calibration_std.nc"
+)
+df_std_ref_sample = ref_std.interp(
+    internal_temperature_bins=df_sample.internal_temperature_bins,
+    method="linear",
+    kwargs={"fill_value": "extrapolate", "bounds_error": False},
 ).drop_vars("internal_temperature_bins")
 
 # %%
+df_sample = df_sample.sel(range=slice(50, 15000))
 df_sample["ppol_c"] = df_sample["ppol_r"] - df_mean_ref_sample["ppol_ref"]
 df_sample["xpol_c"] = df_sample["xpol_r"] - df_mean_ref_sample["xpol_ref"]
 
 df_sample["depo_c"] = df_sample["xpol_c"] / df_sample["ppol_c"]
-
+df_sample["beta_c"] = (df_sample["ppol_c"] + df_sample["xpol_c"]) * (df_sample.range**2)
+df_sample["beta_0"] = (df_sample["ppol_r"] + df_sample["xpol_r"]) * (df_sample.range**2)
+df_sample["depo_0"] = df_sample["xpol_r"] / df_sample["ppol_r"]
 # For depo, no need range correction, as result for uncertainty will be the same
 instrument_1012 = (
     df_std_ref_sample["xpol_r"]
@@ -66,59 +77,144 @@ solar_1012 = xr.where(
 )
 df_sample["ppol_std"] = np.sqrt(df_std_ref_sample["ppol_r"] ** 2 + solar_1012**2)
 
+df_sample["beta_v_std"] = (
+    np.sqrt(df_sample["ppol_std"] ** 2 + df_sample["xpol_std"] ** 2)
+) * df_sample.range**2  #  range correction, think carefully here
+
 df_sample["depo_c_std"] = np.abs(df_sample["depo_c"]) * np.sqrt(
     (df_sample["xpol_std"] / df_sample["xpol_c"]) ** 2
     + (df_sample["ppol_std"] / df_sample["ppol_c"]) ** 2
 )
+# %%
+model = xr.open_dataset(
+    glob.glob(f"/media/viet/CL61/studycase/kenttarova/{date}/weather/*ecmwf.nc")[0]
+)
+model = model.sel(time=time_slice)
+model = model[["temperature", "pressure", "q", "height"]]
+model = model.interp(time=df_sample.time, method="nearest")
+
+
+# %% inversion for aerosol only
+def interp_to_height(x, z, z_new):
+    return np.interp(z_new, z, x)
+
+
+z_new = xr.DataArray(df_sample.range.values, dims=["range"])
+for x in ["q", "temperature", "pressure"]:
+    model[x] = xr.apply_ufunc(
+        interp_to_height,
+        model[x],  # (time, level)
+        model.height,  # (time, level)
+        z_new,  # (range)
+        input_core_dims=[["level"], ["level"], ["range"]],
+        output_core_dims=[["range"]],
+        vectorize=True,
+        dask="parallelized",
+        output_dtypes=[model[x].dtype],
+    )
+    model[x] = model[x].assign_coords(range=z_new)
+
+mol_scatter = rayleigh.molecular_backscatter(
+    2 * np.pi,
+    model["temperature"],
+    model["pressure"] / 100,  # Pa to hPa
+)
+beta_mol = mol_scatter / 1000
+
+depo_mol = rayleigh.depo(
+    rayleigh.f(0.91055, 425, rayleigh.humidity_conversion(model["q"]))
+)
+df_sample["beta_p"] = rayleigh.forward(
+    df_sample.beta_c,
+    beta_mol,
+    50,
+    1 / 1,
+    df_sample.range,
+)
+df_sample["beta_p_std"] = rayleigh.forward_sigma(
+    df_sample["beta_p"],
+    beta_mol,
+    50,
+    1 / 1,
+    df_sample.range,
+    df_sample["beta_v_std"],
+)
 
 # %%
-mask = df_sample["depo_c_std"] < 0.25
-fig, ax = plt.subplots(
-    2, 2, figsize=(9, 6), constrained_layout=True, sharex=True, sharey=True
+depo_volume = df_sample["xpol_c"] / df_sample["ppol_c"]
+beta_ratio = rayleigh.backscatter_ratio(df_sample["beta_p"], beta_mol)
+df_sample["depo_aerosol"] = rayleigh.depo_aerosol(depo_volume, depo_mol, beta_ratio)
+df_sample["depo_aerosol_sigma"] = rayleigh.depo_aersosol_sigma(
+    depo_volume, depo_mol, beta_ratio, df_sample["depo_c_std"], df_sample["beta_p_std"]
 )
 
+# %%
+fig, ax = plt.subplots(
+    4, 2, figsize=(12, 9), constrained_layout=True, sharey="row", sharex=True
+)
+# beta corrected
 p = ax[0, 0].pcolormesh(
-    df_sample["time"],
-    df_sample["range"],
-    (df_sample["p_pol"]).T,
+    df_sample.time,
+    df_sample.range,
+    df_sample["beta_c"].T,
     norm=LogNorm(vmin=1e-7, vmax=1e-4),
 )
-cbar = fig.colorbar(p, ax=ax[0, 0])
-cbar.set_label(r"$ppol$")
-
+fig.colorbar(p, ax=ax[0, 0], label=r"$\beta'_v$")
 p = ax[0, 1].pcolormesh(
-    df_sample["time"],
-    df_sample["range"],
-    (df_sample["x_pol"] / df_sample["p_pol"]).where(mask).T,
-    vmin=0,
-    vmax=0.15,
+    df_sample.time,
+    df_sample.range,
+    (df_sample["beta_v_std"] ** 2).T,
+    norm=LogNorm(vmin=1e-16, vmax=1e-12),
 )
-cbar = fig.colorbar(p, ax=ax[0, 1])
-cbar.set_label(r"$\delta_{original}$")
+fig.colorbar(p, ax=ax[0, 1], label=r"$\sigma^2_{\beta'_v}$")
+ax[0, 1].set_ylim(0, 4000)
 
+# depo corrected
 p = ax[1, 0].pcolormesh(
-    df_sample["time"],
-    df_sample["range"],
-    df_sample["depo_c"].where(mask).T,
-    vmin=0,
-    vmax=0.15,
+    df_sample.time, df_sample.range, df_sample["depo_c"].T, vmin=0, vmax=0.3
 )
-cbar = fig.colorbar(p, ax=ax[1, 0])
-cbar.set_label(r"$\delta_{corrected}$")
-
+fig.colorbar(p, ax=ax[1, 0], label=r"$\delta_{v}$")
 p = ax[1, 1].pcolormesh(
-    df_sample["time"],
-    df_sample["range"],
-    df_sample["depo_c_std"].where(mask).T,
-    vmin=0,
-    vmax=0.15,
+    df_sample.time, df_sample.range, (df_sample["depo_c_std"] ** 2).T, vmin=0, vmax=0.1
 )
-cbar = fig.colorbar(p, ax=ax[1, 1])
-cbar.set_label(r"$\sigma_{\delta}$")
-for ax_ in ax.flatten():
-    ax_.xaxis.set_major_formatter(myFmt)
-    ax_.xaxis.set_major_locator(mdates.HourLocator(interval=1))
-ax[0, 0].set_ylim([100, 8000])
+fig.colorbar(p, ax=ax[1, 1], label=r"$\sigma^2_{\delta_{v}}$")
+ax[1, 1].set_ylim(0, 4000)
+
+# beta aerosol
+p = ax[2, 0].pcolormesh(
+    df_sample.time,
+    df_sample.range,
+    df_sample["beta_p"].T,
+    norm=LogNorm(vmin=1e-7, vmax=1e-4),
+)
+fig.colorbar(p, ax=ax[2, 0], label=r"$\beta_p$")
+p = ax[2, 1].pcolormesh(
+    df_sample.time,
+    df_sample.range,
+    (df_sample["beta_p_std"] ** 2).T,
+    norm=LogNorm(vmin=1e-16, vmax=1e-12),
+)
+fig.colorbar(p, ax=ax[2, 1], label=r"$\sigma^2_{\beta_p}$")
+ax[2, 1].set_ylim(0, 1000)
+
+# depo aerosol
+p = ax[3, 0].pcolormesh(
+    df_sample.time, df_sample.range, df_sample["depo_aerosol"].T, vmin=0, vmax=0.3
+)
+fig.colorbar(p, ax=ax[3, 0], label=r"$\delta_{p}$")
+p = ax[3, 1].pcolormesh(
+    df_sample.time,
+    df_sample.range,
+    (df_sample["depo_aerosol_sigma"] ** 2).T,
+    vmin=0,
+    vmax=0.1,
+)
+fig.colorbar(p, ax=ax[3, 1], label=r"$\sigma^2_{\delta_{p}}$")
+ax[3, 1].set_ylim(0, 1000)
+ax[3, 0].xaxis.set_major_formatter(myFmt)
+ax[3, 0].xaxis.set_major_locator(mdates.HourLocator(interval=1))
+for ax_ in ax[:, 0]:
+    ax_.set_ylabel("Range (m)")
 
 for n, ax_ in enumerate(ax.flatten()):
     ax_.text(
@@ -128,141 +224,53 @@ for n, ax_ in enumerate(ax.flatten()):
         transform=ax_.transAxes,
         size=12,
     )
-ax[0, 0].set_ylabel("Range (m)")
-ax[1, 0].set_ylabel("Range (m)")
 fig.savefig(
     "/media/viet/CL61/img/studycase_depo_corrected.png", bbox_inches="tight", dpi=600
 )
 
 # %%
-error = np.sqrt((df_sample["depo_c_std"] ** 2).sum(dim="time")) / df_sample.time.size
-fig, ax = plt.subplots(2, 4, figsize=(9, 6), sharey=True, constrained_layout=True)
+df_plot = df_sample.sel(time="2024-06-04T13:00", method="nearest")
+fig, ax = plt.subplots(2, 2, figsize=(9, 6), constrained_layout=True, sharey=True)
+ax[0, 0].plot(df_plot["beta_0"], df_plot.range, ".", label=r"$\beta$")
+ax[0, 0].plot(df_plot["beta_c"], df_plot.range, ".", label=r"$\beta_v$")
+ax[0, 0].plot(df_plot["beta_p"], df_plot.range, ".", label=r"$\beta_p$")
+# ax[0, 0].set_xscale('log')
+ax[0, 0].set_xlim(1e-8, 4e-7)
+ax[0, 0].set_xlabel(r"$\beta$")
 
-ppol0 = (df_sample["ppol_r"].mean(dim="time") * df_sample.range**2).where(error < 0.003)
-ax[0, 0].plot(
-    ppol0,
-    df_sample["range"],
-    ".",
-    label="original",
-)
-ppol_c = (df_sample["ppol_c"].mean(dim="time") * df_sample.range**2).where(
-    error < 0.003
-)
-ax[0, 0].plot(
-    ppol_c,
-    df_sample["range"],
-    ".",
-    label="corrected",
-)
-ax[0, 0].set_xlim([2e-7, 2.5e-7])
-ax[0, 0].set_xlabel(r"$\mu_{ppol}$")
-ax[0, 0].legend(loc="upper right")
-ax[1, 0].plot(
-    np.abs(ppol0 - ppol_c) / ppol0,
-    df_sample["range"],
-    ".",
-    label="original",
-)
-ax[1, 0].set_xlim([0, 0.04])
-ax[1, 0].xaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
-ax[1, 0].set_xlabel(r"$\Delta\mu_{ppol}$")
-##########################################################
-xpol0 = (df_sample["xpol_r"].mean(dim="time") * df_sample.range**2).where(error < 0.003)
 ax[0, 1].plot(
-    xpol0,
-    df_sample["range"],
-    ".",
-    label="original",
-)
-
-xpol_c = (df_sample["xpol_c"].mean(dim="time") * df_sample.range**2).where(
-    error < 0.003
+    df_plot["beta_v_std"] ** 2, df_plot.range, ".", label=r"$\sigma^2_{\beta_v}$"
 )
 ax[0, 1].plot(
-    xpol_c,
-    df_sample["range"],
-    ".",
-    label="corrected",
+    df_plot["beta_p_std"] ** 2, df_plot.range, ".", label=r"$\sigma^2_{\beta_p}$"
 )
-ax[0, 1].set_xlim(right=5e-9)
-ax[0, 1].set_xlabel(r"$\mu_{xpol}$")
-ax[0, 1].legend(loc="upper right")
+# ax[0, 1].set_xscale("log")
+ax[0, 1].set_xlim(1e-17, 4e-14)
+ax[0, 1].set_xlabel(r"$\sigma^2_{\beta}$")
+
+ax[1, 0].plot(df_plot["depo_0"], df_plot.range, ".", label=r"$\delta$")
+ax[1, 0].plot(df_plot["depo_c"], df_plot.range, ".", label=r"$\delta_v$")
+ax[1, 0].plot(df_plot["depo_aerosol"], df_plot.range, ".", label=r"$\delta_p$")
+ax[1, 0].set_xlim(0, 0.3)
+ax[1, 0].set_xlabel(r"$\delta$")
+
+
 ax[1, 1].plot(
-    np.abs(xpol0 - xpol_c) / xpol0,
-    df_sample["range"],
-    ".",
+    df_plot["depo_c_std"] ** 2, df_plot.range, ".", label=r"$\sigma^2_{\delta_v}$"
 )
-ax[1, 1].set_xlim([0, 0.8])
-ax[1, 1].xaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-ax[1, 1].set_xlabel(r"$\Delta\mu_{xpol}$")
+ax[1, 1].plot(
+    df_plot["depo_aerosol_sigma"] ** 2,
+    df_plot.range,
+    ".",
+    label=r"$\sigma^2_{\delta_p}$",
+)
+ax[1, 1].set_xlim(0, 0.1)
+ax[1, 1].set_xlabel(r"$\sigma^2_{\delta}$")
 
-###################################################
-beta0 = (df_sample["xpol_r"].mean(dim="time") * df_sample.range**2).where(
-    error < 0.003
-) + (df_sample["ppol_r"].mean(dim="time") * df_sample.range**2).where(error < 0.003)
-beta_new = (df_sample["xpol_c"].mean(dim="time") * df_sample.range**2).where(
-    error < 0.003
-) + (df_sample["ppol_c"].mean(dim="time") * df_sample.range**2).where(error < 0.003)
+for ax_ in ax[:, 0]:
+    ax_.set_ylabel("Range (m)")
 
-ax[0, 2].plot(
-    beta0,
-    df_sample["range"],
-    ".",
-    label="original",
-)
-ax[0, 2].plot(
-    beta_new,
-    df_sample["range"],
-    ".",
-    label="corrected",
-)
-ax[0, 2].set_xlim([2e-7, 2.5e-7])
-ax[0, 2].set_xlabel(r"$\mu_{\beta}$")
-ax[0, 2].legend(loc="upper right")
-ax[1, 2].plot(
-    np.abs(beta0 - beta_new) / beta0,
-    df_sample["range"],
-    ".",
-)
-ax[1, 2].set_xlim([0, 0.04])
-ax[1, 2].xaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
-ax[1, 2].set_xlabel(r"$\Delta\mu_{\beta}$")
-#########################################
-depo0 = (
-    df_sample["xpol_r"].mean(dim="time") / df_sample["ppol_r"].mean(dim="time")
-).where(error < 0.003)
-ax[0, 3].plot(
-    depo0,
-    df_sample["range"],
-    ".",
-    label="original",
-)
-
-depo_c = (
-    df_sample["xpol_c"].mean(dim="time") / df_sample["ppol_c"].mean(dim="time")
-).where(error < 0.003)
-ax[0, 3].errorbar(
-    x=depo_c,
-    y=df_sample.range,
-    xerr=error,
-    fmt=".",
-    label="corrected",
-)
-
-ax[0, 3].set_xlim([-0.005, 0.015])
-ax[0, 3].set_xlabel(r"$\delta$")
-ax[0, 3].legend(loc="upper right")
-ax[1, 3].plot(
-    np.abs(depo0 - depo_c) / depo0,
-    df_sample["range"],
-    ".",
-)
-ax[1, 3].set_xlim([0, 0.8])
-ax[1, 3].xaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-ax[1, 3].set_xlabel(r"$\Delta\delta$")
-#####################
 for n, ax_ in enumerate(ax.flatten()):
-    ax_.grid()
     ax_.text(
         -0.0,
         1.03,
@@ -270,10 +278,12 @@ for n, ax_ in enumerate(ax.flatten()):
         transform=ax_.transAxes,
         size=12,
     )
-ax[0, 0].set_ylim([0, 400])
-ax[0, 0].set_ylabel("Range (m)")
-ax[1, 0].set_ylabel("Range (m)")
+    ax_.legend()
+    ax_.set_ylim(50, 1000)
+    ax_.grid()
 fig.savefig(
-    "/media/viet/CL61/img/studycase_depo_profile.png", bbox_inches="tight", dpi=600
+    "/media/viet/CL61/img/studycase_depo_corrected_profile.png",
+    bbox_inches="tight",
+    dpi=600,
 )
 # %%
